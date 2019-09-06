4cc9e2688f3fd950f0f0018eb9faf54471de1d60
HAFIZI <haziq_yusof@hotmail.com>
2019-09-06 19:49:49 -0400

    change port

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ app.post("/", function(req, res) {
 });
 
 // Finally, start our server
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Telegram app listening on port 3000!");
   registered["cibai"] = "sial";
   console.log(registered);
