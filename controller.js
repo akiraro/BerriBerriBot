d4832ca6e751faf11c7d439cfeaaa30e6c8ab56f
HAFIZI <haziq_yusof@hotmail.com>
2019-09-07 19:24:54 -0400

    trtr

--- controller.js
+++ controller.js
@@ -6,6 +6,10 @@ exports.registerUser = (id, username, res) => {
   const queryString =
     "INSERT INTO `Users` (`id`,`user_id`,`username`) VALUES (?,?,?)";
 
+  connection.on("error", function(err) {
+    console.log("Caught this error: " + err);
+  });
+
   connection.query(
     queryString,
     ["DEFAULT", id, username],
