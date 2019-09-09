0d6aad828155e4c2b2938f924ba39c156cb3e554
HAFIZI <haziq_yusof@hotmail.com>
2019-09-08 22:30:16 -0400

    push

--- index.js
+++ index.js
@@ -171,14 +171,14 @@ app.post("/", function(req, res) {
   }
 });
 
-var CronJob = require("cron").CronJob;
-console.log("Before job instantiation");
-const job = new CronJob("* * * * * *", function() {
-  const d = new Date();
-  console.log("Every second:", d);
-});
-console.log("After job instantiation");
-job.start();
+// var CronJob = require("cron").CronJob;
+// console.log("Before job instantiation");
+// const job = new CronJob("* * * * * *", function() {
+//   const d = new Date();
+//   console.log("Every second:", d);
+// });
+// console.log("After job instantiation");
+// job.start();
 
 // Finally, start our server
 app.listen(process.env.PORT || 3000, function() {
